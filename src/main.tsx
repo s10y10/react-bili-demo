2ce61249397e9183696ff74a2edc6626ed1aa421
yang.song <yang.song@17zuoye.com>
2022-08-15 17:18:20 +0800

    lint: 限制pm

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
-import "./tools/rem";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import 'font-awesome/css/font-awesome.css';
+import './index.css';
+import './tools/rem';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
